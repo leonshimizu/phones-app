26d32caea61174e9a616a708c52b04f5e8089cf6
“leonshimizu” <lmshimizu@gmail.com>
2021-11-30 17:09:04 -0800

    added phones create/new

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -8,4 +8,19 @@ class PhonesController < ApplicationController
     @phone = Phone.find(params[:id])
     render 'show.html.erb'
   end
+
+  def create
+    @phone = Phone.new(
+      maker: params[:maker],
+      version: params[:version],
+      color: params[:color]
+    )
+    @phone.save
+    # render 'show.html.erb'
+    redirect_to "/phones/#{@phone.id}"
+  end
+
+  def new
+    render 'new.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/phones" => "phones#index"
+  get "/phones/new" => "phones#new"
   get "/phones/:id" => "phones#show"
+  post "/phones" => "phones#create"
 end
